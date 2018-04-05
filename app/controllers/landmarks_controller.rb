7a19f62796cf55144bb0a816684791f44e736958
github username <github email address>
2018-04-05 16:04:55 -0400

    adding migrations and working on controllers

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,6 @@
 class LandmarksController < ApplicationController
-
+	#read all landmarks 
+	#create new landmark
+	#when a user creates or edits a figure, 
+	#select or create new landmark or title
 end
---
+++ db/migrate/20180405193929_create_figure.rb
@@ -0,0 +1,8 @@
+class CreateFigure < ActiveRecord::Migration[5.1]
+  def change
+  	create_table(:figures) do |t|
+  		t.string :name
+  		t.timestamps
+  	end
+  end
+end
