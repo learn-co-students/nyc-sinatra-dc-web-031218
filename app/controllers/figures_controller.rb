7d6199cc2a62716bcdcf40267aca9f07c6bd203e
github username <github email address>
2018-04-06 10:21:36 -0400

    all changes

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -9,20 +9,50 @@ class FiguresController < ApplicationController
 
 	#make new figure
 	get '/figures/new' do
+		@titles = Title.all
+		@landmarks = Landmark.all
 		erb '/figures/new'.to_sym
 	end
 
 	#post that creates figures
 	post '/figures' do
-		binding.pry
-		if params[:title] != ""
-			@title = Title.find_or_create_by(params[:title])
-		end
-		@figure = Figure.create(params[:figure])
+		# binding.pry
+		@figure = Figure.find_or_create_by(name: params[:figure][:name])
 
-		#@landmark = Landmark.create? since we need to create or update a landmark
+		if params[:title][:name] != ""
+			title = Title.find_or_create_by(name: params[:title][:name])
+			@figure.titles << title
+		end
+
+		params[:figure][:title_ids][0].keys.each do |title|
+			binding.pry
+			@figure.titles << Title.find_by(name: title)
+		end
+
+		if params[:landmark][:name] != ""
+			landmark = Landmark.find_or_create_by(name: params[:landmark][:name], year_completed: params[:landmark][:year_completed])
+			@figure.landmarks << landmark
+		end
+
+		params[:figure][:landmark_ids][0].keys.each do |landmark|
+			@figure.landmarks << Landmark.find_by(name: landmark)
+		end
+
 		redirect to "/figures/#{@figure.id}"
 	end
+
+	# params
+	# => {"figure"=>
+	#   {"name"=>"HelpMeOut",
+	#    "title_ids"=>
+	#     [{"The Mayor"=>"on", "testingtestintesting"=>"on"}],
+	#    "landmark_ids"=>
+	#     [{"Flatiron School"=>"on", "Subway Station"=>"on"}]},
+	#  "title"=>{"name"=>"OneMoreForToday"},
+	#  "landmark"=>
+	#   {"name"=>"HuuuugeLandmarks", "year_completed"=>"1234"}}
+
+
 
 	#show a specific figure by ID
 	get '/figures/:id' do
@@ -32,7 +62,14 @@ class FiguresController < ApplicationController
 
 	# edit a figure
 	get '/figures/:id/edit' do
-		@figures = Figure.find(params[:id])
+		@figure = Figure.find(params[:id])
+
+		@figure_titles = @figure.titles
+		@titles = Title.all
+
+		@figure_landmarks = @figure.landmarks
+		@landmarks = Landmark.all
+		
 		erb '/figures/edit'.to_sym
 	end
 
