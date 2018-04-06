f1b9e735f94f766f379cad24f7c1b6321834811b
github username <github email address>
2018-04-05 21:02:59 -0400

    how to get data from buttons

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -4,19 +4,22 @@ class FiguresController < ApplicationController
 	# shows all figures
 	get '/figures' do
 		@figures = Figure.all
-
 		erb '/figures/index'.to_sym
 	end
 
 	#make new figure
 	get '/figures/new' do
-		
 		erb '/figures/new'.to_sym
 	end
 
 	#post that creates figures
 	post '/figures' do
+		binding.pry
+		if params[:title] != ""
+			@title = Title.find_or_create_by(params[:title])
+		end
 		@figure = Figure.create(params[:figure])
+
 		#@landmark = Landmark.create? since we need to create or update a landmark
 		redirect to "/figures/#{@figure.id}"
 	end
